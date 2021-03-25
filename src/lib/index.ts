fb5d4de990ddd215b479315092655a7f9dfed2cb
Haixiang <haixiang6123@gmail.com>
2021-03-26 00:34:06 +0800

    feat: 添加 taskTimeout

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,10 +1,11 @@
-import {delay} from './utils'
+import {delay, timeout} from './utils'
 
 interface Options {
   taskFn: Function // 轮询任务
   interval: number // 轮询周期
   masterTimeout?: number // 整个轮询过程的 timeout
   shouldContinue?: (err: string | null, result: any) => boolean // 当次轮询后是否需要继续
+  taskTimeout?: number // 轮询任务的 timeout
 }
 
 const defaultOptions: Partial<Options> = {
@@ -15,7 +16,7 @@ const promisePoller = (options: Options) => {
   // 合并默认设置
   options = {...defaultOptions, ...options}
 
-  const {taskFn, interval, masterTimeout, shouldContinue} = options
+  const {taskFn, interval, masterTimeout, taskTimeout, shouldContinue} = options
 
   let timeoutId: null | number
 
@@ -26,22 +27,34 @@ const promisePoller = (options: Options) => {
       }, masterTimeout)
     }
 
+    // 轮询函数
     const poll = () => {
-      const result = taskFn()
+      let taskPromise = Promise.resolve(taskFn())
 
-      if (shouldContinue(null, result)) {
-        // 继续轮询
-        delay(interval).then(poll)
-      } else {
-        // 不需要轮询，有 timeoutId 则清除
-        if (timeoutId !== null) {
-          clearTimeout(timeoutId)
-        }
-        // 结束并返回最后一次 taskFn 的结果
-        resolve(result)
+      if (taskTimeout) {
+        taskPromise = timeout(taskPromise, taskTimeout)
       }
+
+      taskPromise
+        .then(result => {
+          if (shouldContinue(null, result)) {
+            // 继续轮询
+            delay(interval).then(poll)
+          } else {
+            // 不需要轮询，有 timeoutId 则清除
+            if (timeoutId !== null) {
+              clearTimeout(timeoutId)
+            }
+            // 结束并返回最后一次 taskFn 的结果
+            resolve(result)
+          }
+        })
+        .catch(error => {
+
+        })
     }
 
+    // 第一次轮询
     poll()
   })
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,14 @@
+export const timeout = (promise: Promise<any>, interval: number) => {
+  return new Promise((resolve, reject) => {
+    const timeoutId = setTimeout(() => reject('Task timeout'), interval)
+
+    promise.then(result => {
+      clearTimeout(timeoutId)
+      resolve(result)
+    })
+  })
+}
+
 export const delay = (interval: number) => new Promise(resolve => {
   setTimeout(resolve, interval)
 })
